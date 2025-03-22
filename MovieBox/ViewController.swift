5da00332a6ff850a0da015807f2bbcf0a3488ed2
duy0312 <nguyenduyhd1999@gmail.com>
2025-03-22 07:57:23 +0700

    Initial Commit

---
+++ MovieBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBox
+//
+//  Created by Duy Nguyen on 22/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
